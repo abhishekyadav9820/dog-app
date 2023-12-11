58e543c17e2ed92eae232dc9ab704d65eb08d37d
Abhishek Yadav <89504298+abhishekyadav9820@users.noreply.github.com>
2023-12-11 13:34:26 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import { Card, Row, Image } from "antd";
+import axios from 'axios';
 
 function App() {
+  const [dogImages, setDogImages] = useState([]);
+
+  const fetchDogImages = () => {
+    axios.get("https://dog.ceo/api/breeds/image/random/4")
+      .then(response => setDogImages(response.data.message))
+      .catch(error => console.error('Error fetching dog images:', error));
+  };
+
+  useEffect(() => {
+    fetchDogImages();
+  }, []); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h3>Dog Images</h3>
+        <Card style={{ padding: "20px",display: "flex", justifyContent: "center" }}>
+          <Row gutter={16}>
+            {dogImages && dogImages.map((imageUrl, index) => (
+              <div className="imageCard">
+              <Image
+                key={index}
+                width={300}
+                height={300}
+                src={imageUrl}
+                alt={`Dog ${index + 1}`}
+              />
+              </div>
+            ))}
+          </Row>
+        </Card>
+      </div>
     </div>
   );
 }
